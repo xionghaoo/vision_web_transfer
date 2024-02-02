54386f261f8cf3834a0f67e61ebd98ce147a7e3a
Zero <xionghao930316@gmail.com>
2024-02-02 17:51:18 +0800

    生产环境

--- src/config/host.js
+++ src/config/host.js
@@ -8,9 +8,9 @@
 export const RequestHttpLocal = 'http://192.168.8.130:5002';
 // export const RequestHttpLocal = 'http://rvi.ubtrobot.com:5009';
 
-export const RequestHttpDev = 'http://vision_dev.udicaria.com';
+export const RequestHttpDev = 'https://vision_dev.udicaria.com';
 // export const RequestHttpDev = 'http://192.168.2.187:5003';
 
 //生产环境api地址
-export const RequestHttpPro = 'http://vision.udicaria.com';
+export const RequestHttpPro = 'https://vision.udicaria.com';
 
