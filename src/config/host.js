619a8a4680b4ccc02ea9a346651563576fad31d1
Zero <xionghao930316@gmail.com>
2024-03-04 18:55:20 +0800

    host change

--- src/config/host.js
+++ src/config/host.js
@@ -8,7 +8,7 @@
 export const RequestHttpLocal = 'http://192.168.8.130:5002';
 // export const RequestHttpLocal = 'http://rvi.ubtrobot.com:5009';
 
-export const RequestHttpDev = 'https://vision_dev.udicaria.com';
+export const RequestHttpDev = 'https://vision-dev.udicaria.com';
 // export const RequestHttpDev = 'http://192.168.2.187:5003';
 
 //生产环境api地址
